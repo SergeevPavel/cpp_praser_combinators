f3f4baa9ae5b18b470d8110a978441b034959b66
pavel <sergeev.pavel.al@gmail.com>
2015-03-09 17:32:58 +0300

    Implemented bind and sat combinators

--- Parser.hs
+++ Parser.hs
@@ -1,3 +1,5 @@
+module Parser where
+
 type Parser a = String -> [(a, String)]
 
 result :: a -> Parser a
@@ -13,4 +15,10 @@ item = \inp -> case inp of
 
 pseq :: Parser a -> Parser b -> Parser (a, b)
 p `pseq` q = \inp -> [((v, w), inp'') | (v, inp')  <- p inp
-                                     , (w, inp'') <- q inp']
+                                      , (w, inp'') <- q inp']
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+p `bind` f = \inp -> concat [f v inp' | (v, inp') <- p inp]
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = item `bind` \x -> if p x then result x else zero
