594785665d5b16e11f15079a903a229e345779a1
pavel <sergeev.pavel.al@gmail.com>
2015-04-06 02:44:09 +0300

    added elementary combinators

--- cpp/combinators.h
+++ cpp/combinators.h
@@ -12,10 +12,32 @@ template <class A>
 Parser<A> result(A const v)
 {
     return Parser<A>([&v](typename Parser<A>::parser_input_t input){
-        return std::vector< std::pair<A, std::string> >(1, std::make_pair(v, input));
+        return typename Parser<A>::parser_output_t(1, std::make_pair(v, input));
     });
 }
 
+template <class A>
+Parser<A> zero()
+{
+    return Parser<A>([](typename Parser<A>::parser_input_t input){
+       return typename Parser<A>::parser_output_t();
+    });
+}
+
+Parser<char> item()
+{
+    return Parser<char>([](typename Parser<char>::parser_input_t input)
+    {
+       if (input.length() == 0)
+       {
+           return Parser<char>::parser_output_t();
+       }
+       else
+       {
+           return Parser<char>::parser_output_t(1, std::make_pair(input[0], input.substr(1, input.length() - 1)));
+       }
+    });
+}
 
 #endif // COMBINATORS_H
 
--- cpp/examples/examples.cpp
+++ cpp/examples/examples.cpp
@@ -13,8 +13,38 @@ void print(typename Parser<A>::parser_output_t const& result)
     std::cout << "]" << std::endl;
 }
 
+void result_test()
+{
+    std::cout << "result test:" << std::endl;
+
+    print<int>(result<int>(5).apply("hello"));
+    print<std::string>(result<std::string>("x").apply("hello"));
+
+    std::cout << "----------------" << std::endl;
+}
+
+void zero_test()
+{
+    std::cout << "zero test:" << std::endl;
+
+    print<int>(zero<int>().apply("hello"));
+
+    std::cout << "----------------" << std::endl;
+}
+
+void item_test()
+{
+    std::cout << "item test:" << std::endl;
+
+    print<char>(item().apply("hello"));
+
+    std::cout << "----------------" << std::endl;
+}
+
 int main()
 {
-    print<int>(result<int>(5).apply("hello"));
+    result_test();
+    zero_test();
+    item_test();
     return 0;
 }
