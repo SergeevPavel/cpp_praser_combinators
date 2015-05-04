bb6fd718ad9a27cc0c04e5e246d2a997ea58fde4
pavel <sergeev.pavel.al@gmail.com>
2015-05-04 18:51:06 +0300

    add sepby combinator

--- cpp/combinators.h
+++ cpp/combinators.h
@@ -13,7 +13,7 @@ template <class A>
 Parser< std::list<A> > many(Parser<A> const& p)
 {
     return  p       >= [p](const A x)
-    {return many(p) >= [x](std::list<A> xs){
+    {return many(p) >= [x](std::list<A> xs){ // TODO perf try to add reference
      xs.push_front(x);
      return result(xs);};} || result(std::list<A>());
 }
@@ -22,12 +22,26 @@ template <class A>
 Parser< std::list<A> > many1(Parser<A> const& p)
 {
     return p       >= [p](const A x){
-    return many(p) >= [x](std::list<A> xs){
+    return many(p) >= [x](std::list<A> xs){ // TODO perf try to add reference
     xs.push_front(x);
     return result(xs);};};
 }
 
+template <class A, class B>
+Parser< std::list<A> > sepby1(Parser<A> const& p, Parser<B> const& sep) {
+    const Parser<A> chunk = (sep >= [p](const B _){
+                    return   p   >= [](const A x){
+                    return   result(x);};});
+    return p           >= [chunk](const A x){
+    return many(chunk) >= [x](std::list<A> xs){
+    xs.push_front(x);
+    return result(xs);};};
+}
 
+template <class A, class B>
+Parser< std::list<A> > sepby(Parser<A> const p, Parser<B> const& sep) {
+    return sepby1(p, sep) || result(std::list<A>());
+}
 
 #endif // COMBINATORS_H
 
